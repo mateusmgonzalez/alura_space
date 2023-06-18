ca7a26419e45e0c64963d4ece8415b59ffae2526
mateusmgonzalez <mateusmgonzalez@gmail.com>
2023-06-18 00:08:11 -0300

    Add css e ajustando os templates

--- galeria/views.py
+++ galeria/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
